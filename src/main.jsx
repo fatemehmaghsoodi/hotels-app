7eb4c2401a6d452aeeb80c1a0e6dce0e3fde2bc0
fatemehmaghsoodi <fatemehhmaghsoodi@gmail.com>
2025-01-13 19:12:58 +0330

    App.scss name fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './assets/sass/app.scss'
+import './assets/sass/App.scss'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
